1f4a8d9b10ad34a4330b139c9f6cbdfee64cbef2
RomeuTavares <141793676+RomeuTavares@users.noreply.github.com>
2023-10-10 21:50:26 -0300

    Add files via upload

---
+++ Frm_Aula1/Faculdade/frm_Aula1/script.js
@@ -0,0 +1,6 @@
+function cadastro(){ 
+
+nome = document.getElementById("nome_aluno").value;
+email = document.getElementById("email_aluno").value;
+document.getElementById("resultado").innerHTML = "Nome do aluno: " + nome + "<br> e email: " + email
+}
